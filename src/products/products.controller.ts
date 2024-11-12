cdd4c0044ec4c8021f45c57b80d30e14b6d0cb54
Hovhannisyan00 <ars.hovhannisyan00@gmail.com>
2024-11-12 22:10:28 +0400

    Swager

--- src/products/products.controller.ts
+++ src/products/products.controller.ts
@@ -8,18 +8,62 @@ import {
 } from '@nestjs/common';
 import { ProductsService } from './products.service';
 import { CreateProductDto } from './dto/create-category.dto';
+import { ApiBody, ApiOperation, ApiResponse, ApiTags } from '@nestjs/swagger';
 
+@ApiTags('products')
 @Controller('api')
 export class ProductsController {
   constructor(private readonly productsService: ProductsService) {}
 
+  @ApiOperation({ summary: 'Create a new product' })
+  @ApiBody({
+    description: 'Product data to create a new product',
+    type: CreateProductDto,
+    examples: {
+      example1: {
+        summary: 'Sample product data',
+        value: {
+          title: 'Nstaran',
+          description: 'Baskator: 2, sexan1',
+          price: 20000,
+          category: 'Home Appliances',
+        },
+      },
+    },
+  })
+  @ApiResponse({
+    status: 201,
+    description: 'Successfully created the product',
+    schema: {
+      example: {
+        title: 'Nstaran',
+        description: 'Baskator: 2, sexan1',
+        price: 20000,
+        category: 'Home Appliances',
+      },
+    },
+  })
+  @Post('/products')
+  async createProduct(@Body() createProductDto: CreateProductDto) {
+    return await this.productsService.createProduct(createProductDto);
+  }
+
+  @ApiOperation({ summary: 'Find product by ID' })
+  @ApiResponse({
+    status: 200,
+    description: 'Successfully retrieved the product',
+    schema: {
+      example: {
+        id: 1,
+        title: 'Nstaran',
+        description: 'Baskator: 2, sexan1',
+        price: 20000,
+        category: 'Home Appliances',
+      },
+    },
+  })
   @Get('/products/:id')
   async findByID(@Param('id', ParseIntPipe) id: number) {
     return await this.productsService.findByID(id);
-  }
-
-  @Post('/products')
-  async createProduct(@Body() CreateProductDto: CreateProductDto) {
-    return await this.productsService.createProduct(CreateProductDto);
   }
 }
